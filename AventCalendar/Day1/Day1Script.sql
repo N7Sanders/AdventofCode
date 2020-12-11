5388ae884dcce4a463a206ec92fcd6df7484697b
N7Sanders <75820004+N7Sanders@users.noreply.github.com>
2020-12-10 20:47:58 -0600

    Add files via upload

---
+++ AventCalendar/Day1/Day1Script.sql
@@ -0,0 +1,18 @@
+--Day1
+DROP TABLE IF EXISTS #Day1
+
+CREATE TABLE #Day1 (Day1 INT)
+
+BULK INSERT #Day1 FROM 'C:\Temp\AventCalendar\Day1\Day1Data.txt'
+
+SELECT DISTINCT(a.Day1 * b.Day1) as 'ANSWER 1A'
+FROM #Day1 a
+JOIN #Day1 b on 1=1 
+WHERE a.Day1 + b.Day1 = 2020
+
+--Day1B
+SELECT DISTINCT(a.Day1 * b.Day1 * c.Day1) as 'ANSWER 1B'
+FROM #Day1 a
+JOIN #Day1 b on 1=1
+JOIN #Day1 c on 1=1
+WHERE a.Day1 + B.Day1 + c.Day1 = 2020
